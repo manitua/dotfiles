efb335ef5ffeb1f2fce0505683a0c9a36cb5cecc
Jasmin Beharic <jasmin.beharic@gmail.com>
2024-10-13 01:00:16 +0200

    Add vim-fugitive plugin to neovim
    Signed-off-by: Jasmin Beharic <jasmin.beharic@gmail.com>

---
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "tpope/vim-fugitive",
+    event = { "BufReadPre", "BufNewFile" },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+      signs_staged = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    },
+  },
+}

44a9de165c1cc8886a45e91b55d6e0fcb3d7ec58
Jasmin Beharic <jasmin.beharic@gmail.com>
2024-10-12 19:52:59 +0200

    Show current buffer attached LSP client in lualine
    Signed-off-by: Jasmin Beharic <jasmin.beharic@gmail.com>

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,21 @@ return {
     local lualine = require "lualine"
     local lazy_status = require "lazy.status" -- to configure lazy pending updates count
 
+    -- LSP clients attached to buffer
+    local clients_lsp = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      local clients = vim.lsp.get_clients { bufnr = bufnr }
+      if next(clients) == nil then
+        return ""
+      end
+
+      local c = {}
+      for _, client in pairs(clients) do
+        table.insert(c, client.name)
+      end
+      return "\u{f085} " .. table.concat(c, "|")
+    end
+
     lualine.setup {
       options = {
         theme = "catppuccin",
@@ -17,6 +32,10 @@ return {
             lazy_status.updates,
             cond = lazy_status.has_updates,
             color = { fg = "#fab387" },
+          },
+          {
+            clients_lsp,
+            color = { fg = "#89b4fa" },
           },
           { "encoding" },
           { "fileformat" },

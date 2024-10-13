6af449795cbc51dd71a1f9e0da8d12076c955530
Jasmin Beharic <jasmin.beharic@gmail.com>
2024-10-13 17:08:31 +0200

    Add ToggleTerm to Neovim
    Signed-off-by: Jasmin Beharic <jasmin.beharic@gmail.com>

---
+++ nvim/.config/nvim/lua/plugins/terminal.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    lazy = true,
+    version = "*",
+    opts = {},
+    keys = {
+      { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "[T]oggle terminal in [F]loating window" },
+    },
+  },
+}

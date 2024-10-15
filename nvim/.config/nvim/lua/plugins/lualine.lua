d390d8a05271cb64877a2a8046dd012b8bea7cbc
Jasmin Beharic <jasmin.beharic@gmail.com>
2024-10-15 22:48:51 +0200

    Add Mason "packages to update" in Lualine
    Signed-off-by: Jasmin Beharic <jasmin.beharic@gmail.com>

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -20,6 +20,28 @@ return {
       return "\u{f085} " .. table.concat(c, "|")
     end
 
+    local registry = require "mason-registry"
+
+    local mason_has_updates = function()
+      local installed_packages = registry.get_installed_package_names()
+      local pkgs_to_upgrade = 0
+
+      local function callback(success)
+        if success then
+          pkgs_to_upgrade = pkgs_to_upgrade + 1
+        end
+      end
+
+      for _, pkg in pairs(installed_packages) do
+        local p = registry.get_package(pkg)
+        if p then
+          p:check_new_version(callback)
+        end
+      end
+
+      return pkgs_to_upgrade ~= 0 and "\u{f019} " .. pkgs_to_upgrade or ""
+    end
+
     lualine.setup {
       options = {
         theme = "catppuccin",
@@ -32,6 +54,10 @@ return {
             lazy_status.updates,
             cond = lazy_status.has_updates,
             color = { fg = "#fab387" },
+          },
+          {
+            mason_has_updates,
+            color = { fg = "#f38ba8" },
           },
           {
             clients_lsp,

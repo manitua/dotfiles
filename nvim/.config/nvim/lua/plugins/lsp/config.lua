71c8530df4f42ef4278fb53bf36b95841365c835
Jasmin Beharic <jasmin.beharic@gmail.com>
2025-02-03 22:37:58 +0100

    Use luals 3rd library for luv
    Signed-off-by: Jasmin Beharic <jasmin.beharic@gmail.com>

--- nvim/.config/nvim/lua/plugins/lsp/config.lua
+++ nvim/.config/nvim/lua/plugins/lsp/config.lua
@@ -7,11 +7,10 @@ return { -- LSP Plugins
     opts = {
       library = {
         -- Load luvit types when the `vim.uv` word is found
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       },
     },
   },
-  { "Bilal2453/luvit-meta", lazy = true },
   {
     -- Main LSP Configuration
     "neovim/nvim-lspconfig",

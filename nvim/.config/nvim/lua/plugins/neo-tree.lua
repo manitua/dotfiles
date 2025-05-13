efb6997ef441d70be922af2b83f5319e3fc6159c
Jasmin Beharic <jasmin.beharic@gmail.com>
2025-05-13 21:20:16 +0200

    Do not lazy-load neo-tree
    Signed-off-by: Jasmin Beharic <jasmin.beharic@gmail.com>

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,22 +1,23 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
+  version = "*",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  config = function()
-    local keymap = vim.keymap
-    keymap.set("n", "<C-n>", "<cmd>Neotree filesystem reveal left toggle=true<CR>", { desc = "Toggle file explorer" })
-
-    require("neo-tree").setup {
+  lazy = false,
+  keys = {
+    { "<C-n>", "<cmd>Neotree filesystem reveal left toggle=true<CR>", desc = "Toggle file explorer" },
+  },
+  opts = {
+    filesystem = {
       window = {
         mappings = {
           ["<cr>"] = "open",
           ["<tab>"] = "open",
         },
       },
-    }
-  end,
+    },
+  },
 }

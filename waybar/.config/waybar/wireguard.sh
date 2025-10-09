31d26f8c06456b435ebd967d1e19dd0c308bb786
Jasmin Beharic <jasmin.beharic@fabled.se>
2025-10-09 16:33:38 +0200

    Add wireguard to waybar
    Signed-off-by: Jasmin Beharic <jasmin.beharic@fabled.se>

---
+++ waybar/.config/waybar/wireguard.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+ip -details -json link show type wireguard | jq -r -c --unbuffered '
+  [.[] | .ifname] |
+  if length != 0 then
+    if length == 1 then
+      {text: .[], alt: "up", class: "wireguard"}
+    else
+      {text: "#" + (length | tostring), alt: "up", class: "wireguard"}
+    end
+  else
+    {text: "", alt: "down", class: "wireguard"}
+  end
+'
